276406bf526a861a5b8c930a36729f01c6d2b144
vimfun <vimfunny@gmail.com>
2019-10-29 21:55:50 +0800

    Step 4.1: curry the function `modifyModelFields`

--- FunctionalProgramming.java
+++ FunctionalProgramming.java
@@ -25,18 +25,20 @@ class FunctionalProgramming {
     }
 
     public static Map<String, Class> addModelFields(Map<String, Class> ctm) {
-        return modifyModelFields(ctm, ctm2 -> f -> ctm2.put(f, String.class));
+        return modifyModelFields(ctm).apply(ctm2 -> f -> ctm2.put(f, String.class));
     }
 
     public static Map<String, Class> removeModelFields(Map<String, Class> ctm) {
-        return modifyModelFields(ctm, ctm2 -> f -> ctm2.remove(f));
+        return modifyModelFields(ctm).apply(ctm2 -> f -> ctm2.remove(f));
     }
 
-    private static Map<String, Class> modifyModelFields(Map<String, Class> ctm, Function<Map<String, Class>, Consumer<String>> ffn) {
-        Array<String> fields = Array.of( "pay_time", "created_time" );
-        Map<String, Class> ctm2 = new HashMap<>();
-        ctm2.putAll(ctm);
-        fields.forEach( ffn.apply(ctm2) );
-        return ctm2;
+    private static Function<Function<Map<String, Class>, Consumer<String>>, Map<String, Class>> modifyModelFields(Map<String, Class> ctm) {
+        return ffn -> {
+            Array<String> fields = Array.of( "pay_time", "created_time" );
+            Map<String, Class> ctm2 = new HashMap<>();
+            ctm2.putAll(ctm);
+            fields.forEach( ffn.apply(ctm2) );
+            return ctm2;
+        };
     }
 }
