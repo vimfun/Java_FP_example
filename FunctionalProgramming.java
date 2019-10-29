acf8aed91bce52468dc46d069e8a75fff6e8395e
vimfun <vimfunny@gmail.com>
2019-10-29 21:38:26 +0800

    Step 3.1: use `forEach` just for elegant.
        (but not total functional)

--- FunctionalProgramming.java
+++ FunctionalProgramming.java
@@ -24,60 +24,17 @@ class FunctionalProgramming {
 
     public static Map<String, Class> addModelFields(Map<String, Class> ctm) {
         Array<String> fields = Array.of( "pay_time", "created_time" );
-
-        Map<String, Class> _ctm = fields.toJavaStream()
-        
-            .map(f -> new Tuple2<String, Class>(f, String.class))
-
-            .reduce(
-                new HashMap<String, Class>(),
-
-                (m, fc) -> {
-                    m.put(fc._1, fc._2);
-                    return m;
-                },
-
-                (m1, m2) -> {
-                    HashMap<String, Class> m = new HashMap<>();
-                    m.putAll(m1);
-                    m.putAll(m2);
-                    return m;
-                }
-            );
-
         Map<String, Class> ctm2 = new HashMap<>();
         ctm2.putAll(ctm);
-        ctm2.putAll(_ctm);
+        fields.forEach(f -> ctm2.put(f, String.class));
         return ctm2;
     }
 
     public static Map<String, Class> removeModelFields(Map<String, Class> ctm) {
         Array<String> fields = Array.of( "pay_time", "created_time" );
-
-        Map<String, Class> _ctm = fields.toJavaStream()
-        
-            .map(f -> new Tuple2<String, Class>(f, String.class))
-
-            .reduce(
-                new HashMap<String, Class>(),
-
-                (m, fc) -> {
-                    m.put(fc._1, fc._2);
-                    return m;
-                },
-
-                (m1, m2) -> {
-                    HashMap<String, Class> m = new HashMap<>();
-                    m.putAll(m1);
-                    m.putAll(m2);
-                    return m;
-                }
-            );
-
         Map<String, Class> ctm2 = new HashMap<>();
         ctm2.putAll(ctm);
         fields.forEach(f -> ctm2.remove(f));
-
         return ctm2;
     }
 }
