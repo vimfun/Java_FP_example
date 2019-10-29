a2fc608f74845ab936911d4b4ebac158f089897d
vimfun <vimfunny@gmail.com>
2019-10-29 15:09:10 +0800

    Step 2.0   `map` and `reduce`

--- FunctionalProgramming.java
+++ FunctionalProgramming.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import io.vavr.collection.Array;
+import io.vavr.Tuple2;
 
 class FunctionalProgramming {
 
@@ -9,9 +10,27 @@ class FunctionalProgramming {
 
         Array<String> fields = Array.of( "pay_time", "created_time" );
 
-        fields.forEach(
-            f -> ctm.put(f, String.class)
-        );
+        Map<String, Class> _ctm = fields.toJavaStream()
+        
+            .map(f -> new Tuple2<String, Class>(f, String.class))
+
+            .reduce(
+                new HashMap<String, Class>(),
+
+                (m, fc) -> {
+                    m.put(fc._1, fc._2);
+                    return m;
+                },
+
+                (m1, m2) -> {
+                    HashMap<String, Class> m = new HashMap<>();
+                    m.putAll(m1);
+                    m.putAll(m2);
+                    return m;
+                }
+            );
+
+        ctm.putAll( _ctm );
 
         System.out.printf("ctm  ->  ");
         System.out.println(ctm);
